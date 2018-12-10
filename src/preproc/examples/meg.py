786f085df7d9343bc6026c0e6d519e1432b8f258
Noam Peled <peled.noam@gmail.com>
2018-12-10 16:51:24 -0500

    meg clusterings

--- src/preproc/examples/meg.py
+++ src/preproc/examples/meg.py
@@ -347,27 +347,35 @@ def calc_msit_functional_rois(args):
 
 
 def calc_sample_clusters(args):
+    import numpy as np
+    import os
+
     clusters_root_fol = utils.make_dir(op.join(MMVT_DIR, 'sample', 'meg', 'clusters'))
-    utils.delete_folder_files(clusters_root_fol)
-    _args = meg.read_cmd_args(dict(
-        subject=args.subject,
-        mri_subject=args.mri_subject,
-        task='audvis',
-        conditions=['LA', 'RA'],
-        function='find_functional_rois_in_stc',
-        stc_name='sample_audvis-meg',
-        inv_fname='sample_audvis-meg-eeg-oct-6-meg-eeg-inv',
-        label_name_template='*',
-        peak_stc_time_index=10,
-        # peak_mode='pos',
-        threshold=9,#99.5,
-        threshold_is_precentile=False,
-        # min_cluster_max=5,
-        min_cluster_size=100,
-        # recreate_src_spacing='ico5'
-        # clusters_label='precentral'
-    ))
-    meg.call_main(_args)
+    for threshold in np.arange(2, 9.6, 0.1):
+        utils.delete_folder_files(clusters_root_fol)
+        _args = meg.read_cmd_args(dict(
+            subject=args.subject,
+            mri_subject=args.mri_subject,
+            task='audvis',
+            conditions=['LA', 'RA'],
+            function='find_functional_rois_in_stc',
+            stc_name='sample_audvis-meg',
+            inv_fname='sample_audvis-meg-eeg-oct-6-meg-eeg-inv',
+            label_name_template='*',
+            peak_stc_time_index=10,
+            # peak_mode='pos',
+            threshold=threshold,#99.5,
+            threshold_is_precentile=False,
+            # min_cluster_max=5,
+            min_cluster_size=100,
+            # recreate_src_spacing='ico5'
+            # clusters_label='precentral'
+        ))
+        meg.call_main(_args)
+        os.rename(op.join(clusters_root_fol, 'sample_audvis-meg'),
+                  op.join(clusters_root_fol, 'sample_audvis-meg-{:.2f}'.format(threshold)))
+        os.rename(op.join(clusters_root_fol, 'clusters_labels_sample_audvis-meg.pkl'),
+                  op.join(clusters_root_fol, 'clusters_labels_sample_audvis_{:.2f}-meg.pkl'.format(threshold)))
 
 
 if __name__ == '__main__':
