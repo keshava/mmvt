0bd1aff6752fa378614c989ea719425e08558ed2
Noam Peled <peled.noam@gmail.com>
2019-02-28 22:37:54 -0500

    ela morphing

--- src/examples/ela_morph_electrodes.py
+++ src/examples/ela_morph_electrodes.py
@@ -67,6 +67,7 @@ def calc_subcorticals_pos(subject, aseg_data, lut):
 
 
 @utils.timeit
+@utils.tryit()
 def calc_elas(subject, specific_elecs_names, template, template_header, bipolar=False,  atlas='aparc.DKTatlas',
               error_radius=3, elc_length=4, print_warnings=False, overwrite=False, n_jobs=1):
     fol = utils.make_dir(op.join(MMVT_DIR, subject, 'electrodes'))
@@ -78,11 +79,12 @@ def calc_elas(subject, specific_elecs_names, template, template_header, bipolar=
                   elec_name, elec_pos, elec_dist, elec_type, elec_ori in \
                   zip(elecs_names, elecs_pos, elecs_dists, elecs_types, elecs_oris)
                   if elec_name in specific_elecs_names]
-    labels_vertices, regions_center_of_mass, regions_names, aseg_data, lut, pia_verts = init(subject, atlas, n_jobs)
-    template_labels_vertices, template_regions_center_of_mass, template_regions_names, template_aseg_data, lut, template_pia_verts = init(template, atlas, n_jobs)
+    (labels_vertices, regions_center_of_mass, regions_names, aseg_data, lut, pia_verts) = init(
+        subject, atlas, n_jobs)
+    (template_labels_vertices, template_regions_center_of_mass, template_regions_names, template_aseg_data, lut,
+     template_pia_verts) = init(template, atlas, n_jobs)
     len_lh_pia = len(pia_verts['lh'])
     template_len_lh_pia = len(template_pia_verts['lh'])
-    # subject_header = nib.load(op.join(SUBJECTS_DIR, subject, 'mri', 'T1.mgz')).header
     epsilon = 0
     max_run_num = 1000
     parallel = True
@@ -103,8 +105,9 @@ def calc_elas(subject, specific_elecs_names, template, template_header, bipolar=
         elec_labeling_template = calc_ela(
             template, bipolar, elec_name, template_elec_pos, elec_type, elec_ori, elec_dist, template_labels_vertices, template_aseg_data, lut,
             template_pia_verts, template_len_lh_pia, args.excludes, error_radius, elc_length, print_warnings, overwrite, n_jobs)
-        err = comp_elecs_labeling(elec_labeling_no_whites, elec_labeling_template, regions_center_of_mass,
-            regions_names, template_regions_center_of_mass, template_regions_names, subject_prob_pos_in_template_space)
+        err = comp_elecs_labeling(
+            elec_labeling_template, template_regions_center_of_mass, template_regions_names,
+            subject_prob_pos_in_template_space)
         run_num = 0
         stop_gradient = False
         print(err)
@@ -136,9 +139,9 @@ def calc_elas(subject, specific_elecs_names, template, template_header, bipolar=
                         template_labels_vertices, template_aseg_data, lut,
                         template_pia_verts, template_len_lh_pia, args.excludes, error_radius, elc_length,
                         print_warnings, overwrite, n_jobs)
-                    new_err = comp_elecs_labeling(elec_labeling_no_whites, elec_labeling_template, regions_center_of_mass,
-                                              regions_names, template_regions_center_of_mass, template_regions_names,
-                                              subject_prob_pos_in_template_space)
+                    new_err = comp_elecs_labeling(
+                        elec_labeling_template, template_regions_center_of_mass, template_regions_names,
+                        subject_prob_pos_in_template_space)
                     if new_err < err:
                         new_template_pos = new_template_elec_pos
                         err = new_err
@@ -174,8 +177,8 @@ def _parallel_calc_ela_err(p):
         template_labels_vertices, template_aseg_data, lut,
         template_pia_verts, template_len_lh_pia, excludes, error_radius, elc_length, overwrite, 1)
     err = comp_elecs_labeling(
-        elec_labeling_no_whites, elec_labeling_template, regions_center_of_mass, regions_names,
-        template_regions_center_of_mass, template_regions_names, subject_prob_pos)
+        elec_labeling_template, template_regions_center_of_mass, template_regions_names,
+        subject_prob_pos)
     return err
 
 
@@ -192,31 +195,13 @@ def calc_prob_pos(elec_labeling_no_whites, regions_center_of_mass, regions_names
     return np.sum(elec_prob_dist, axis=0)
 
 
-def comp_elecs_labeling(elec_labeling_no_whites, elec_labeling_template, subject_regions_center_of_mass,
-                        subject_regions_names, template_regions_center_of_mass, template_regions_names, subject_prob_pos):
-    # template_first_region, template_first_prob = template_regions_tup[0]
+def comp_elecs_labeling(elec_labeling_template, template_regions_center_of_mass, template_regions_names,
+                        subject_prob_pos):
     template_elec_lebeling_no_whites = calc_elec_labeling_no_white(elec_labeling_template)
-    template_prob_pos = calc_prob_pos(template_elec_lebeling_no_whites, template_regions_center_of_mass, template_regions_names)
-    # subject_com = [subject_regions_center_of_mass[subject_regions_names.index(r)] for r, p in elec_labeling_no_whites]
-    # template_com = [template_regions_center_of_mass[template_regions_names.index(r)] for r, p in template_elec_lebeling_no_whites]
+    template_prob_pos = calc_prob_pos(
+        template_elec_lebeling_no_whites, template_regions_center_of_mass, template_regions_names)
     err = pdist([subject_prob_pos, template_prob_pos])[0]
     return err
-    # dists = cdist(subject_com, template_com)
-    # argmin = np.argmin(dists, axis=1)
-    err = 0
-    
-            
-    for region_ind in regions_indices:
-        region, prob = elc_labeling['regions'][region_ind], elc_labeling['regions_probs'][region_ind]
-        # Check the region is not white matter
-        if region in ['Left-Cerebral-White-Matter', 'Right-Cerebral-White-Matter']:
-            continue
-        # Check if it doesn't appear in the template
-        if region not in elc_labeling_template['regions']:
-            # Calc panalty using dists
-            dist = pdist([template_elec_pos, regions_center_of_mass[regions_names.index(template_first_region)]])
-            return dist * template_first_prob
-        return 0
 
 
 def calc_ela(subject, bipolar, elec_name, elec_pos, elec_type, elec_ori, elec_dist, labels, aseg_data, lut, pia_verts,
@@ -236,90 +221,6 @@ def calc_ela(subject, bipolar, elec_name, elec_pos, elec_type, elec_ori, elec_di
     ret = {'name': elec_name, 'regions': regions + subcortical_regions,
            'regions_probs': regions_probs, 'hemi': hemi}
     return ret
-
-
-def calc_template_elec_vox(subject, template, elec_pos, subject_header=None, template_header=None):
-    if subject_header is None:
-        subject_header = nib.load(op.join(SUBJECTS_DIR, subject, 'mri', 'T1.mgz')).get_header()
-    if template_header is None:
-        template_header = nib.load(op.join(SUBJECTS_DIR, template, 'mri', 'T1.mgz')).get_header()
-    vox = utils.apply_trans(np.linalg.inv(subject_header.get_vox2ras_tkr()), elec_pos)
-    ras = utils.apply_trans(subject_header.get_vox2ras(), vox)
-    template_vox = np.rint(utils.apply_trans(template_header.get_ras2vox(), ras).astype(int))
-    return template_vox
-
-
-# def compare_electrodes_labeling(subject, electrodes_names, template, atlas='aparc.DKTatlas'):
-#     # template_elab_files = glob.glob(op.join(
-#     #     MMVT_DIR, template, 'electrodes', '{}_{}_electrodes_cigar_r_3_l_4.pkl'.format(template, atlas)))
-#     # if len(template_elab_files) == 0:
-#     #     print('No electrodes labeling file for {}!'.format(template))
-#     #     return
-#     # elab_template = utils.load(template_elab_files[0])
-#     errors = ''
-#     elab_files = glob.glob(op.join(
-#         MMVT_DIR, subject, 'electrodes', '{}_{}_electrodes_cigar_r_3_l_4.pkl'.format(subject, atlas)))
-#     if len(elab_files) == 0:
-#         print('No electrodes labeling file for {}!'.format(subject))
-#         return
-#     elab = utils.load(elab_files[0])
-#     # elab = [e for e in elab if e['name'] in electrodes_names]
-#     for elc in electrodes_names:
-#         no_errors = True
-#         elc_labeling = [e for e in elab if e['name'] == elc][0]
-#         elc_labeling_template = [e for e in elab_template if e['name'] == '{}_{}'.format(subject, elc)][0]
-#         for roi, prob in zip(elc_labeling['cortical_rois'], elc_labeling['cortical_probs']):
-#             no_err, err = compare_rois_and_probs(
-#                 subject, template, elc, roi, prob, elc_labeling['cortical_rois'],
-#                 elc_labeling_template['cortical_rois'], elc_labeling_template['cortical_probs'])
-#             no_errors = no_errors and no_err
-#             if err != '':
-#                 errors += err + '\n'
-#         for roi, prob in zip(elc_labeling['subcortical_rois'], elc_labeling['subcortical_probs']):
-#             no_err, err = compare_rois_and_probs(
-#                 subject, template, elc, roi, prob, elc_labeling['subcortical_rois'],
-#                 elc_labeling_template['subcortical_rois'], elc_labeling_template['subcortical_probs'])
-#             no_errors = no_errors and no_err
-#             if err != '':
-#                 errors += err + '\n'
-#         if no_errors:
-#             print('{},{},Good!'.format(subject, elc))
-#             errors += '{},{},Good!\n'.format(subject, elc)
-
-
-def compare_rois_and_probs(subject, template, elc, roi, prob, elc_labeling_rois, elc_labeling_template_rois,
-                           elc_labeling_template_rois_probs):
-    no_errors = True
-    err = ''
-    if roi not in elc_labeling_template_rois:
-        if prob > 0.05:
-            err = f'{subject},{elc},{roi} ({prob}) not in {template}'
-            print(err)
-            no_errors = False
-    else:
-        roi_ind = elc_labeling_template_rois.index(roi)
-        template_roi_prob = elc_labeling_template_rois_probs[roi_ind]
-        if abs(prob - template_roi_prob) > 0.05:
-            err = f'{subject},{elc},{roi} prob ({prob} != {template} prob ({template_roi_prob})'
-            print(err)
-            no_errors = False
-    for roi, prob in zip(elc_labeling_template_rois, elc_labeling_template_rois_probs):
-        if roi not in elc_labeling_rois and prob > 0.05:
-            err = f'{subject},{elc},{roi} ({prob}) only in {template}'
-            print(err)
-            no_errors = False
-    return no_errors, err
-
-
-def robust_register_to_template(subject, subject_to, subjects_dir, vox2vox=False, print_only=False):
-    cmd = mri_robust_register
-    lta_name = 't1_to_{}'.format(subject_to)
-    if vox2vox:
-        cmd += ' --vox2vox'
-        lta_name += '_vox2vox'
-    rs = utils.partial_run_script(locals(), print_only=print_only)
-    rs(cmd)
-
 
 
 if __name__ == '__main__':
--- src/misc/read_electrodes_from_xls.py
+++ src/misc/read_electrodes_from_xls.py
@@ -1,15 +1,17 @@
 import os.path as op
+import nibabel as nib
 from collections import defaultdict
 from src.utils import utils
 from src.utils import preproc_utils as pu
 from src.examples import morph_electrodes_to_template
+from src.examples import ela_morph_electrodes
 
 SUBJECTS_DIR, MMVT_DIR, FREESURFER_HOME = pu.get_links()
 
 
-def read_xls(xls_fname, subject_to='colin27'):
-    template_system = 'mni'
+def read_xls(xls_fname, subject_to='colin27',atlas='aparc.DKTatlas40', check_morph_file=False):
     bipolar = True
+    template_header = nib.load(op.join(SUBJECTS_DIR, subject_to, 'mri', 'T1.mgz')).header
     subjects_electrodes = defaultdict(list)
     electrodes_colors = defaultdict(list)
     for line in utils.xlsx_reader(xls_fname, skip_rows=1):
@@ -17,17 +19,25 @@ def read_xls(xls_fname, subject_to='colin27'):
         subject = subject.replace('\'', '')
         if subject == '':
             break
-        electrodes_fname = op.join(MMVT_DIR, subject, 'electrodes', 'electrodes_morph_to_{}.txt'.format(subject_to))
-        if op.isfile(electrodes_fname):
-            elec_group, num1, num2 = utils.elec_group_number(elec_name, bipolar)
-            if '{}{}-{}'.format(elec_group, num2, num1) != elec_name:
-                num1, num2 = str(num1).zfill(2), str(num2).zfill(2)
-            if '{}{}-{}'.format(elec_group, num2, num1) != elec_name:
-                raise Exception('Wrong group or numbers!')
-            for num in [num1, num2]:
-                subjects_electrodes[subject].append('{}{}'.format(elec_group, num))
-            electrodes_colors[subject].append((elec_name, int(anat_group)))
+        if check_morph_file:
+            electrodes_fname = op.join(MMVT_DIR, subject, 'electrodes', 'electrodes_morph_to_{}.txt'.format(subject_to))
+            if not op.isfile(electrodes_fname):
+                continue
+        elec_group, num1, num2 = utils.elec_group_number(elec_name, bipolar)
+        if '{}{}-{}'.format(elec_group, num2, num1) != elec_name:
+            num1, num2 = str(num1).zfill(2), str(num2).zfill(2)
+        if '{}{}-{}'.format(elec_group, num2, num1) != elec_name:
+            raise Exception('Wrong group or numbers!')
+        for num in [num1, num2]:
+            subjects_electrodes[subject].append('{}{}'.format(elec_group, num))
+        electrodes_colors[subject].append((elec_name, int(anat_group)))
     subjects = list(subjects_electrodes.keys())
+    for subject in subjects:
+        ela_morph_electrodes.calc_elas(
+            subject, elec_name, subject_to, template_header, bipolar=False, atlas=atlas)
+
+
+def morph_csv():
     electrodes = morph_electrodes_to_template.read_all_electrodes(subjects, False)
     template_electrodes = morph_electrodes_to_template.read_morphed_electrodes(
         electrodes, template_system, SUBJECTS_DIR, MMVT_DIR, True, subjects_electrodes, convert_to_bipolar=bipolar)
@@ -38,5 +48,8 @@ def read_xls(xls_fname, subject_to='colin27'):
 
 
 if __name__ == '__main__':
-    xls_fname = '/home/cashlab/Documents/noam/Onset_regions_for_illustration.xlsx'
+    fols = ['C:\\Users\\peled\\Documents\\Pariya', '/home/cashlab/Documents/noam/']
+    fol = [f for f in fols if op.isdir(f)][0]
+    xls_fname = op.join(fol, 'Onset_regions_for_illustration.xlsx')
+
     read_xls(xls_fname)
