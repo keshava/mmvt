13b59fe59d529382c09e712960fe9618336ebbe2
Noam Peled <peled.noam@gmail.com>
2019-04-16 15:34:22 -0400

    create full connectivity

--- src/examples/scripts/create_all_electrodes_connectivity.py
+++ src/examples/scripts/create_all_electrodes_connectivity.py
@@ -20,34 +20,39 @@ def run(mmvt):
     conn_parent_obj = bpy.data.objects[conn_parent_name]
     layers_rods = [False] * 20
     layers_rods[mmvt.CONNECTIONS_LAYER] = True
+    create_all_connections(mmvt, parent, conn_parent_obj, orig_curve, layers_rods)
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.data.objects[orig_curve.name].select = True
+    bpy.ops.object.delete()
+
+
+def create_all_connections(mmvt, parent, conn_parent_obj, orig_curve, layers_rods):
+    mu = mmvt.utils
     N = len(parent.children)
     con = np.full((N, N), False, dtype=bool)
     now = time.time()
     runs = int(N * N / 2)
     run_ind = 0
-    for ind1, elc_obj1 in enumerate(parent.children):
-        for ind2, elc_obj2 in enumerate(parent.children):
+    # children = parent.children
+    children = [bpy.data.objects[elc_name] for elc_name in [
+        'LOF11-LOF10', 'LAF8-LAF7', 'LAT5-LAT4', 'RPF2-RPF1', 'RPF7-RPF6', 'LOF4-LOF3', 'LAT3-LAT2']]
+    for ind1, elc_obj1 in enumerate(children):
+        for ind2, elc_obj2 in enumerate(children):
             if ind1 == ind2:
                 continue
             if con[ind1, ind2] or con[ind2, ind1]:
                 continue
-            group1, _, _  = mu.elec_group_number(elc_obj1.name, True)
+            group1, _, _ = mu.elec_group_number(elc_obj1.name, True)
             group2, _, _ = mu.elec_group_number(elc_obj2.name, True)
             if group1 == group2:
                 continue
             mu.time_to_go(now, run_ind, runs, 100)
             con[ind1, ind2] = True
-            # mu.create_bezier_curve(elc_obj1, elc_obj2, layers_rods)
             cur_obj = copy_curve(
-                orig_curve, elc_obj1, elc_obj2, conn_parent_name, layers_rods, bevel_depth=0.1, resolution_u=1)
+                orig_curve, elc_obj1, elc_obj2, conn_parent_obj.name, layers_rods, bevel_depth=0.1, resolution_u=1)
             cur_obj.name = '{}-{}'.format(elc_obj1.name, elc_obj2.name)
             cur_obj.parent = conn_parent_obj
             run_ind += 1
-            # mu.create_and_set_material(cur_obj)
-
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects[orig_curve.name].select = True
-    bpy.ops.object.delete()
 
 
 def copy_curve(orig_curve, obj1, obj2, parent_name, layers_array, bevel_depth=0.1, resolution_u=1):
