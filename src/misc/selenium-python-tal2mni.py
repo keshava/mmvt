0a81f2f8bc63b6de79ad424a330c41a55deda713
pelednoam <peled.noam@gmail.com>
2019-06-11 16:39:24 -0400

    tal2mni

--- src/misc/selenium-python-tal2mni.py
+++ src/misc/selenium-python-tal2mni.py
@@ -4,57 +4,56 @@ from src.utils import utils
 import csv
 import os.path as op
 from tqdm import tqdm
+import glob
 
 SUBJECTS_DIR, MMVT_DIR, FREESURFER_HOME = pu.get_links()
 
 
-def trans_tal_coords(roi, file_name, subjects_dir, template='colin27', overwrite=False):
-    subjects = {}
+def trans_tal_coords(files, template='colin27', overwrite=False):
     output_fol = utils.make_dir(op.join(MMVT_DIR, template, 'rois_peaks'))
-    csv_fname = op.join(output_fol, '{}.csv'.format(roi))
-    pkl_fname = op.join(output_fol, '{}.pkl'.format(roi))
-    if op.isfile(pkl_fname) and op.isfile(csv_fname) and not overwrite:
-        print('Data already exist for {}'.format(roi))
-        return
-    driver = tu.yale_get_driver()
-    files = list(utils.find_recursive(subjects_dir, file_name))
+    output_fname = op.join(output_fol, 'rois.pkl')
+    if not op.isfile(output_fname) or overwrite:
+        rois= get_tal_coordaintes(files)
+        utils.save(rois, output_fname)
+    else:
+        rois = utils.load(output_fname)
+    for roi in rois.keys():
+        csv_fname = op.join(output_fol, '{}.csv'.format(roi))
+        with open(csv_fname, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            for mni_coordinates in rois[roi]['mni']:
+                csv_writer.writerow(mni_coordinates)
+
+
+def get_tal_coordaintes(files):
+    rois, errors = {}, {}
+    driver = None
     for fname in tqdm(files):
+        # subject = utils.namebase(utils.get_parent_fol(fname, 3))
+        roi = utils.namebase(fname).split('.')[0]
+        if roi not in rois:
+            rois[roi] = {}
+            rois[roi]['tal'] = []
+            rois[roi]['mni'] = []
         lines = list(utils.csv_file_reader(fname, delimiter=' '))
-        subject = utils.namebase(utils.get_parent_fol(fname, 3))
-        subjects[subject] = {}
         if len(lines) == 0:
-            print()
-            subjects[subject]['error'] = '{} is empty!'.format(fname)
+            errors[fname] = '{} is empty!'.format(fname)
             continue
         elif len(lines) > 1:
-            print('More than one line in {}!'.format(fname))
-            subjects[subject] = '>1'
+            errors[fname] = 'More than one line in {}!'.format(fname)
             continue
         tal = [int(float(v)) for v in lines[0] if utils.is_float(v)]
-        subjects[subject]['tal'] = tal
-        subjects[subject]['mni'] = tu.yale_tal2mni(tal, driver)
+        rois[roi]['tal'].append(tal)
+        if driver is None:
+            driver = tu.yale_get_driver()
+        rois[roi]['mni'].append(tu.yale_tal2mni(tal, driver))
+    if len(errors) > 0:
+        print(errors)
     del driver
-    print(subjects)
-    with open(csv_fname, 'w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',')
-        for subject, subject_data in subjects.items():
-            if 'mni' in subject_data:
-                csv_writer.writerow(subjects[subject]['mni'])
-    utils.save(subjects, pkl_fname)
+    return rois
 
 
 if __name__ == '__main__':
-    '''
-    `cd /autofs/space/lilli_004/users/DARPA-MSIT/msit/subjs`
-    `find . -name "dACC.txt"`
-    `find . -name "L_dlPFC.txt"`
-    `find . -name "R_dlPFC.txt"`
-    `find . -name "L_IFG.txt"`
-    `find . -name "R_IFG.txt"`
-    '''
-    subjects_dir = '/autofs/space/lilli_004/users/DARPA-MSIT/msit/subjs'
-    rois = ['dACC', 'L_dlPFC', 'R_dlPFC', 'L_IFG', 'R_IFG']
-    files = ['{}.txt'.format(roi) for roi in rois]
-    for roi, file_name in zip(rois, files):
-        print(roi)
-        trans_tal_coords(roi, file_name, subjects_dir, 'colin27', False)
+    subjects_dir = '/autofs/space/lilli_004/users/DARPA-MSIT/msit/subjs/'
+    files = glob.glob(op.join(subjects_dir, '**', '*3dExtrema.txt'), recursive=True)
+    trans_tal_coords(files, 'colin27', False)
