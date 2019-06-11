b9677f06f351370f90299eb60f084267c7b3e4e8
pelednoam <peled.noam@gmail.com>
2019-06-11 15:53:55 -0400

    Fixing the normals

--- src/examples/scripts/fix_subcorticals_normals.py
+++ src/examples/scripts/fix_subcorticals_normals.py
@@ -6,11 +6,13 @@ def run(mmvt):
     for obj in bpy.data.objects:
         obj.select = False
     bm = bmesh.new()
-    for obj in bpy.data.objects['Subcortical_structures'].children:
-        mesh = obj.data
-        bm.from_mesh(mesh)
-        bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
-        bm.to_mesh(mesh)
-        bm.clear()
-        mesh.update()
+    for root_folder in ['Subcortical_structures', 'Subcortical_fmri_activity_map', 'Subcortical_meg_activity_map']:
+        for obj in bpy.data.objects[root_folder].children:
+            mesh = obj.data
+            bm.from_mesh(mesh)
+            print('Recalc normals for {}'.format(obj.name))
+            bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+            bm.to_mesh(mesh)
+            bm.clear()
+            mesh.update()
     bm.free()
