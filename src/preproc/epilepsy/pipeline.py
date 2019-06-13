3ff67a71e973bf66a02b7bd143d522d036b840fa
pelednoam <peled.noam@gmail.com>
2019-06-13 16:55:39 -0400

    epilepsy

--- src/preproc/epilepsy/pipeline.py
+++ src/preproc/epilepsy/pipeline.py
@@ -3,6 +3,7 @@ from src.preproc import meg
 from src.utils import utils
 from src.preproc.epilepsy import plots
 from src.preproc.epilepsy import power_spectrums_plots as psplots
+from src.preproc.epilepsy import utils as epi_utils
 import glob
 import os.path as op
 import mne
@@ -220,7 +221,7 @@ def _calc_max_powers_parallel(p):
         print('{}: Not all the files were created!'.format(fol))
         return
     print('Calculating max power for {}'.format(fol))
-    max_powers = np.max(concatenate_powers(fol), axis=0)
+    max_powers = np.max(epi_utils.concatenate_powers(fol), axis=0)
     print('Saving to {}'.format(output_fname))
     np.save(output_fname, max_powers)
 
@@ -259,67 +260,6 @@ def _calc_induced_power_zvals_parallel(p):
         module.call_main(args)
 
 
-def move_non_zvals_stcs(subject, modality):
-    # Move not zvals stc files
-    modality_fol = op.join(MMVT_DIR, subject, 'eeg' if modality == 'eeg' else 'meg')
-    non_zvlas_fol = utils.make_dir(op.join(modality_fol, 'non-zvals'))
-    stc_files = [f for f in glob.glob(op.join(modality_fol, '*.stc'))
-                 if '-epilepsy-' in utils.namebase(f) and not '-zvals-' in utils.namebase(f)]
-    for stc_fname in stc_files:
-        utils.move_file(stc_fname, non_zvlas_fol, overwrite=True)
-
-
-def fix_amplitude_fnames(subject, bands):
-    stcs_files = glob.glob(op.join(MMVT_DIR, subject, 'meg', '{}-epilepsy-*-zvals-?h.stc'.format(subject))) + \
-                 glob.glob(op.join(MMVT_DIR, subject, 'eeg', '{}-epilepsy-*-zvals-?h.stc'.format(subject)))
-    for stc_fname in stcs_files:
-        stc_name = utils.namebase(stc_fname)[len('{}-epilepsy-'.format(subject)):-len('-zvals-lh')]
-        if stc_name.endswith('amplitude'):
-            continue
-        if not any([stc_name.endswith(band) for band in bands]):
-            stc_name += '_amplitude'
-            stc_end = '-zvals-lh.stc' if stc_fname.endswith('-zvals-lh.stc') else '-zvals-rh.stc'
-            new_stc_fname = op.join(
-                utils.get_parent_fol(stc_fname), '{}-epilepsy-{}{}'.format(subject, stc_name, stc_end))
-            print('{} -> {}'.format(stc_fname, new_stc_fname))
-            utils.rename_files(stc_fname, new_stc_fname)
-
-
-def add_run_number_to_files(subject, run):
-    run_num = re.sub('\D', ',', run).split(',')[-1]
-    files = glob.glob(op.join(MEG_DIR, subject, '{}-epilepsy-*.fif'.format(subject))) + \
-            glob.glob(op.join(EEG_DIR, subject, '{}-epilepsy-*.fif'.format(subject)))
-            # glob.glob(op.join(MMVT_DIR, subject, 'meg', '{}-epilepsy-*.fif'.format(subject))) + \
-            # glob.glob(op.join(MMVT_DIR, subject, 'eeg', '{}-epilepsy-*.fif'.format(subject)))
-    for fname in files:
-        new_fname = fname.replace('-epilepsy-', '-epilepsy{}-'.format(run_num))
-        print('{} - > {}'.format(fname, new_fname))
-        utils.rename_files(fname, new_fname)
-
-
-def create_evokeds_links(subject, windows):
-    fol = utils.make_dir(op.join(MMVT_DIR, subject, 'evoked'))
-    for window_fname in windows:
-        new_window_fname = op.join(fol, utils.namebase_with_ext(window_fname))
-        if op.isfile(new_window_fname) or op.islink(new_window_fname):
-            continue
-        utils.make_link(window_fname, new_window_fname)
-
-
-def find_vertices(subject, modality='meg', atlas='aparc.DKTatlas'):
-    from src.utils import labels_utils as lu
-    root_dir = op.join(EEG_DIR if modality == 'eeg' else MEG_DIR, subject)
-    inv_fname = op.join(root_dir, '{}-epilepsy{}-{}-inv.fif'.format(subject, run_num, modality))
-    inv = mne.minimum_norm.read_inverse_operator(inv_fname)
-    labels = lu.read_labels(subject, SUBJECTS_DIR, atlas)
-    vertices = []
-    for ind, label in enumerate(labels):
-        _, vertno = mne.minimum_norm.inverse.label_src_vertno_sel(label, inv['src'])
-        vertices.extend(vertno)
-    np.save(op.join(MMVT_DIR, subject, 'labels_verts.npy'), np.array(vertices))
-    return vertices
-
-
 def calc_stc_power_specturm(subject, modality, window_fname, baseline_window, run_num, inverse_method='dSPM',
                             atlas='aparc.DKTatlas', high_gamma_max=120):
     from src.utils import labels_utils as lu
@@ -335,7 +275,7 @@ def calc_stc_power_specturm(subject, modality, window_fname, baseline_window, ru
 
     d = np.load(window_output_fname)
     min_indeices, max_indices = d['min_vertices'], d['max_vertices']
-    times = get_window_times(window_fname, downsample=2)
+    times = epi_utils.get_window_times(window_fname, downsample=2)
     t_max = np.where(times > 0.15)[0][0]
     t_min = np.where(times > 0)[0][0]
     x = d['max'][:12, t_min:t_max]
@@ -349,7 +289,7 @@ def calc_stc_power_specturm(subject, modality, window_fname, baseline_window, ru
     labels = lu.read_labels(subject, SUBJECTS_DIR, atlas)
     # max_vert_label = find_vert_label(vertices_ind, modality, window, labels, inv)
     vertices_ind_to_no_lookup, vertices_labels_lookup = \
-        calc_vertices_lookup_tables(modality, window, labels, inv)
+        epi_utils.calc_vertices_lookup_tables(subject, modality, window, inverse_method, labels, inv)
     max_vert_label = vertices_labels_lookup[vertices_ind]
     print('{:.4f} in {}Hz {:.4f}s in {}'.format(max_val, freqs[f], times[t], max_vert_label.name))
 
@@ -368,10 +308,10 @@ def calc_stc_power_specturm(subject, modality, window_fname, baseline_window, ru
         all_files_exist = all_files_exist and op.isfile(label_fname)
 
     if not all_files_exist:
-        baseline = concatenate_powers(baseline_fol) # (vertices x freqs x time)
+        baseline = epi_utils.concatenate_powers(baseline_fol) # (vertices x freqs x time)
         baseline_std = np.std(baseline, axis=2, keepdims=True) # the standard deviation (over time) of log baseline values
         baseline_mean = np.mean(baseline, axis=2, keepdims=True) # the mean (over time) of log baseline values
-        powers = concatenate_powers(powers_fol)
+        powers = epi_utils.concatenate_powers(powers_fol)
         norm_powers = (powers - baseline_mean) / baseline_std
 
     # norm_labels_fol = op.join(root_dir, '{}-epilepsy-{}-{}-{}-induced_power'.format(
@@ -421,34 +361,6 @@ def calc_stc_power_specturm(subject, modality, window_fname, baseline_window, ru
             modality, window, times[t], freqs[f], inverse_method))
     print('Saving stc file for t {} and f {} in {}'.format(t, f, output_stc_fname))
     combined_stc.save(output_stc_fname)
-
-
-def calc_vertices_lookup_tables(modality, window, labels, inv):
-    output_fname = op.join(MMVT_DIR, subject, 'vertices_lookup_tables.pkl')
-    # if op.isfile(output_fname):
-    #     vertices_ind_to_no_lookup, vertices_no_to_ind_lookup, vertices_labels_lookup = utils.load(output_fname)
-    #     return vertices_lookup, vertices_labels_lookup
-    root_dir = op.join(EEG_DIR if modality == 'eeg' else MEG_DIR, subject)
-    powers_fol = op.join(root_dir, '{}-epilepsy-{}-{}-{}-induced_power'.format(
-        subject, inverse_method, modality, window))
-    powers_files = glob.glob(op.join(powers_fol, 'epilepsy_*_induced_power.npy'))
-    start_ind = 0
-    vertice_label = None
-    vertices_ind_to_no_lookup = {}
-    vertices_labels_lookup = {}
-    for file_ind, powers_fname in enumerate(powers_files):
-        label_name = utils.namebase(powers_fname).split('_')[1]
-        label = [l for l in labels if l.name == label_name][0]
-        vertno, src_sel = mne.minimum_norm.inverse.label_src_vertno_sel(label, inv['src'])
-        for vert_ind, vert_num in zip(range(start_ind, start_ind + len(src_sel)), vertno[0] if label.hemi == 'lh' else vertno[1]):
-            vertices_ind_to_no_lookup[vert_ind] = vert_num
-            vertices_labels_lookup[vert_ind] = label
-        # if start_ind <= vertices_ind < start_ind + len(src_sel):
-        #     vertice_label = label
-        #     break
-        start_ind += len(src_sel)
-    utils.save((vertices_ind_to_no_lookup, vertices_labels_lookup), output_fname)
-    return vertices_ind_to_no_lookup, vertices_labels_lookup
 
 
 # @utils.profileit(root_folder=op.join(MMVT_DIR, 'profileit'))
@@ -511,7 +423,7 @@ def main(subject, run, modalities, bands, evokes_fol, raw_fname, empty_fname, ba
         # calc_stc_power_specturm(subject, modality, windows[0], baseline_window, run_num)
         pass
 
-    # find_vertices(subject)
+    # find_vertices(subject, run_num)
     # for window_fname in windows:
     #     figure_name = '' #'{}-modalities-power-spectrum-with-grpahs'.format(utils.namebase(window_fname))
     #     psplots.plot_modalities_power_spectrums_with_graph(subject, modalities, window_fname, figure_name, file_type='eps')
--- src/preproc/epilepsy/utils.py
+++ src/preproc/epilepsy/utils.py
@@ -2,9 +2,16 @@ import numpy as np
 from numpy.ma import masked_array
 import glob
 import os.path as op
+import re
 import mne
 
 from src.utils import utils
+
+LINKS_DIR = utils.get_links_dir()
+MMVT_DIR = utils.get_link_dir(LINKS_DIR, 'mmvt')
+MEG_DIR = utils.get_link_dir(LINKS_DIR, 'meg')
+EEG_DIR = utils.get_link_dir(LINKS_DIR, 'eeg')
+SUBJECTS_DIR = utils.get_link_dir(LINKS_DIR, 'subjects')
 
 
 def calc_powers_abs_minmax(powers, label_norm_powers_files=None, both_min_and_max=True):
@@ -78,3 +85,92 @@ def nans(shape, dtype=np.float32):
     x = np.empty(shape, dtype)
     x.fill(np.nan)
     return x
+
+
+def calc_vertices_lookup_tables(subject, modality, window, inverse_method, labels, inv):
+    output_fname = op.join(MMVT_DIR, subject, 'vertices_lookup_tables.pkl')
+    # if op.isfile(output_fname):
+    #     vertices_ind_to_no_lookup, vertices_no_to_ind_lookup, vertices_labels_lookup = utils.load(output_fname)
+    #     return vertices_lookup, vertices_labels_lookup
+    root_dir = op.join(EEG_DIR if modality == 'eeg' else MEG_DIR, subject)
+    powers_fol = op.join(root_dir, '{}-epilepsy-{}-{}-{}-induced_power'.format(
+        subject, inverse_method, modality, window))
+    powers_files = glob.glob(op.join(powers_fol, 'epilepsy_*_induced_power.npy'))
+    start_ind = 0
+    vertice_label = None
+    vertices_ind_to_no_lookup = {}
+    vertices_labels_lookup = {}
+    for file_ind, powers_fname in enumerate(powers_files):
+        label_name = utils.namebase(powers_fname).split('_')[1]
+        label = [l for l in labels if l.name == label_name][0]
+        vertno, src_sel = mne.minimum_norm.inverse.label_src_vertno_sel(label, inv['src'])
+        for vert_ind, vert_num in zip(range(start_ind, start_ind + len(src_sel)), vertno[0] if label.hemi == 'lh' else vertno[1]):
+            vertices_ind_to_no_lookup[vert_ind] = vert_num
+            vertices_labels_lookup[vert_ind] = label
+        # if start_ind <= vertices_ind < start_ind + len(src_sel):
+        #     vertice_label = label
+        #     break
+        start_ind += len(src_sel)
+    utils.save((vertices_ind_to_no_lookup, vertices_labels_lookup), output_fname)
+    return vertices_ind_to_no_lookup, vertices_labels_lookup
+
+
+def find_vertices(subject, run_num, modality='meg', atlas='aparc.DKTatlas'):
+    from src.utils import labels_utils as lu
+    root_dir = op.join(EEG_DIR if modality == 'eeg' else MEG_DIR, subject)
+    inv_fname = op.join(root_dir, '{}-epilepsy{}-{}-inv.fif'.format(subject, run_num, modality))
+    inv = mne.minimum_norm.read_inverse_operator(inv_fname)
+    labels = lu.read_labels(subject, SUBJECTS_DIR, atlas)
+    vertices = []
+    for ind, label in enumerate(labels):
+        _, vertno = mne.minimum_norm.inverse.label_src_vertno_sel(label, inv['src'])
+        vertices.extend(vertno)
+    np.save(op.join(MMVT_DIR, subject, 'labels_verts.npy'), np.array(vertices))
+    return vertices
+
+
+def fix_amplitude_fnames(subject, bands):
+    stcs_files = glob.glob(op.join(MMVT_DIR, subject, 'meg', '{}-epilepsy-*-zvals-?h.stc'.format(subject))) + \
+                 glob.glob(op.join(MMVT_DIR, subject, 'eeg', '{}-epilepsy-*-zvals-?h.stc'.format(subject)))
+    for stc_fname in stcs_files:
+        stc_name = utils.namebase(stc_fname)[len('{}-epilepsy-'.format(subject)):-len('-zvals-lh')]
+        if stc_name.endswith('amplitude'):
+            continue
+        if not any([stc_name.endswith(band) for band in bands]):
+            stc_name += '_amplitude'
+            stc_end = '-zvals-lh.stc' if stc_fname.endswith('-zvals-lh.stc') else '-zvals-rh.stc'
+            new_stc_fname = op.join(
+                utils.get_parent_fol(stc_fname), '{}-epilepsy-{}{}'.format(subject, stc_name, stc_end))
+            print('{} -> {}'.format(stc_fname, new_stc_fname))
+            utils.rename_files(stc_fname, new_stc_fname)
+
+
+def add_run_number_to_files(subject, run):
+    run_num = re.sub('\D', ',', run).split(',')[-1]
+    files = glob.glob(op.join(MEG_DIR, subject, '{}-epilepsy-*.fif'.format(subject))) + \
+            glob.glob(op.join(EEG_DIR, subject, '{}-epilepsy-*.fif'.format(subject)))
+            # glob.glob(op.join(MMVT_DIR, subject, 'meg', '{}-epilepsy-*.fif'.format(subject))) + \
+            # glob.glob(op.join(MMVT_DIR, subject, 'eeg', '{}-epilepsy-*.fif'.format(subject)))
+    for fname in files:
+        new_fname = fname.replace('-epilepsy-', '-epilepsy{}-'.format(run_num))
+        print('{} - > {}'.format(fname, new_fname))
+        utils.rename_files(fname, new_fname)
+
+
+def create_evokeds_links(subject, windows):
+    fol = utils.make_dir(op.join(MMVT_DIR, subject, 'evoked'))
+    for window_fname in windows:
+        new_window_fname = op.join(fol, utils.namebase_with_ext(window_fname))
+        if op.isfile(new_window_fname) or op.islink(new_window_fname):
+            continue
+        utils.make_link(window_fname, new_window_fname)
+
+
+def move_non_zvals_stcs(subject, modality):
+    # Move not zvals stc files
+    modality_fol = op.join(MMVT_DIR, subject, 'eeg' if modality == 'eeg' else 'meg')
+    non_zvlas_fol = utils.make_dir(op.join(modality_fol, 'non-zvals'))
+    stc_files = [f for f in glob.glob(op.join(modality_fol, '*.stc'))
+                 if '-epilepsy-' in utils.namebase(f) and not '-zvals-' in utils.namebase(f)]
+    for stc_fname in stc_files:
+        utils.move_file(stc_fname, non_zvlas_fol, overwrite=True)
