40df73e5595a7f22c0d8ded6759e53f26f67bc8f
pelednoam <peled.noam@gmail.com>
2019-06-21 16:38:10 -0400

    msit-ecr

--- src/examples/msit_ecr.py
+++ src/examples/msit_ecr.py
@@ -159,6 +159,7 @@ def calc_fwd_inv(args):
         empty_fnames, cors, days = get_empty_fnames(subject, args.tasks, args)
         good_subject = True
         for task in args.tasks:
+            utils.make_dir(op.join(MEG_DIR, task.upper(), subject, 'bem'))
             meg_args = meg.read_cmd_args(dict(
                 subject=args.subject, mri_subject=args.subject,
                 task=task, inverse_method=inv_method, atlas=atlas,
