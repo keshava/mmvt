953b98196a2de1e8b3cfc0acb31a310ba04d6ea7
pelednoam <peled.noam@gmail.com>
2019-06-21 16:41:41 -0400

    msit-ecr

--- src/examples/msit_ecr.py
+++ src/examples/msit_ecr.py
@@ -159,6 +159,8 @@ def calc_fwd_inv(args):
         empty_fnames, cors, days = get_empty_fnames(subject, args.tasks, args)
         good_subject = True
         for task in args.tasks:
+            if op.exists(op.join(MEG_DIR, task.upper(), subject, 'bem')):
+                os.remove(op.join(MEG_DIR, task.upper(), subject, 'bem'))
             utils.make_dir(op.join(MEG_DIR, task.upper(), subject, 'bem'))
             meg_args = meg.read_cmd_args(dict(
                 subject=args.subject, mri_subject=args.subject,
