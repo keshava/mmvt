bb5ef56ffebcfd8aa1468600e33ee2c9a8c4e28e
pelednoam <peled.noam@gmail.com>
2019-07-12 23:53:36 -0400

    epilepsy

--- src/examples/epilepsy/pipeline.py
+++ src/examples/epilepsy/pipeline.py
@@ -679,7 +679,7 @@ def calc_labels_connectivity(
     epochs = epi_utils.combine_windows_into_epochs(windows, op.join(
         root_dir, '{}-{}-{}-{}-{}-epo.fif'.format(subject, modality, atlas, inverse_method, condition)))
     freqs = meg.calc_morlet_freqs(epochs, n_cycles=2, max_high_gamma=120)
-    bands = epi_utils.calc_bands(freqs[0], high_freq, as_dict=False)
+    bands = epi_utils.calc_bands(freqs[0], high_freq)
 
     meg.calc_labels_connectivity(
         subject, atlas, {condition:1}, subjects_dir=SUBJECTS_DIR, mmvt_dir=MMVT_DIR, inverse_method=inverse_method,
