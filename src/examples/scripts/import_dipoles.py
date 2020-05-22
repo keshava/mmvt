d2f1c6a7a470b3150212efba06f551dd92079d79
Eitan Yoshpe <eitan.yoshpe@gmail.com>
2020-05-22 16:50:26 -0400

    Read dipoles from file.

--- src/examples/scripts/import_dipoles.py
+++ src/examples/scripts/import_dipoles.py
@@ -17,13 +17,13 @@ def run(mmvt):
     layers_array[mmvt.ELECTRODES_LAYER] = True
     for dipole_name, dipoles in dipoles_dict.items():
         for dipole_ind, dipole in enumerate(dipoles):
-            dipole_name = 'dipole_{}_{}'.format(dipole_name, dipole_ind)
+            dipole_obj_name = 'dipole_{}_{}'.format(dipole_name, dipole_ind)
             begin_t, end_t, x, y, z, q, qx, qy, qz, gf = dipole
             dipole_loc = Vector((x, y, z)) * world_matrix * 1000
             dipole_dir = Vector((qx, qy, qz)) * world_matrix * 1000
             # dipole_obj = mu.draw_arrow(global_dipole_ind, dipole_loc, dipole_dir)
-            mu.create_sphere(dipole_loc, 0.15, layers_array, dipole_name)
-            dipole_obj = bpy.data.objects[dipole_name]
+            mu.create_sphere(dipole_loc, 0.15, layers_array, dipole_obj_name)
+            dipole_obj = bpy.data.objects[dipole_obj_name]
             dipole_obj.select = True
             dipole_obj.parent = parent_obj
             mu.create_and_set_material(dipole_obj)
