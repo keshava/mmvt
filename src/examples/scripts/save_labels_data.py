248467eab9ad105cef23f658ce49eb2d02cee297
pelednoam <peled.noam@gmail.com>
2020-05-27 16:35:30 -0400

    plot labels data

--- src/examples/scripts/save_labels_data.py
+++ src/examples/scripts/save_labels_data.py
@@ -3,9 +3,7 @@ import os.path as op
 
 def run(mmvt):
     mu = mmvt.utils
-    # labels_data = glob.glob(op.join(mu.get_user_fol(), 'labels', 'labels_data', '*.npz'))
-    labels_data_files = mmvt.labels.get_labels_data_files()
-    for label_data_fname in labels_data_files:
+    for label_data_fname in mmvt.labels.get_labels_data_files():
         label_data_name = mu.namebase(label_data_fname).replace('_', ' ')
         figures_fol = mu.make_dir(op.join(mu.get_user_fol(), 'figures', label_data_name))
         mmvt.coloring.clear_colors()
