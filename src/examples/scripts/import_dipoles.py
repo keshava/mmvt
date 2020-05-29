369a69dc62cff460c7e2f76bb0cc63e9fd34740b
Eitan Yoshpe <eitan.yoshpe@gmail.com>
2020-05-29 17:30:50 -0400

    starts implementing draw dipoles

--- src/examples/scripts/import_dipoles.py
+++ src/examples/scripts/import_dipoles.py
@@ -8,6 +8,10 @@ def _mmvt():
     return ScriptsPanel.addon
 
 
+def dipoles_names_update(self, context):
+    selected_cluster_name = bpy.context.scene.dipoles_names
+
+
 def run(mmvt):
     mu = mmvt.utils
     parent_obj = mu.create_empty_if_doesnt_exists('dipoles', mmvt.MEG_LAYER, None, 'Functional maps')
@@ -16,27 +20,31 @@ def run(mmvt):
         print('No dipoles file!')
         return
     dipoles_dict = ScriptsPanel.dipoles_dict #mu.load(dipoles_fname)
-    global_dipole_ind = 0
     world_matrix = mu.get_matrix_world()
     layers_array = [False] * 20
     layers_array[mmvt.MEG_LAYER] = True
-    show_as_arrows = False
+    show_as_arrows = True
+    color = (1, 0, 0, 1)
     for dipole_name, dipoles in dipoles_dict.items():
         for dipole_ind, dipole in enumerate(dipoles):
             dipole_obj_name = 'dipole_{}_{}'.format(dipole_name, dipole_ind)
             begin_t, end_t, x, y, z, q, qx, qy, qz, gf = dipole
             dipole_loc = Vector((x, y, z)) * world_matrix * 1000
-            ori = Vector((qx, qy, qz)) / (1 / 1e9)
-            dipole_dir = ori * world_matrix * 1000
-            if show_as_arrows:
-                dipole_obj = mu.draw_arrow(global_dipole_ind, dipole_loc, dipole_dir)
-            else:
-                mu.create_sphere(dipole_loc, 0.15, layers_array, dipole_obj_name)
-                dipole_obj = bpy.data.objects[dipole_obj_name]
-            dipole_obj.select = True
-            dipole_obj.parent = parent_obj
+            print(dipole_loc)
+            ori = Vector((qx, qy, qz))
+            dipole_dir = ori * world_matrix * 15
+            print(dipole_dir)
+            print(dipole_loc + dipole_dir)
+            # dipole_obj = draw_arrow(global_dipole_ind, dipole_loc, dipole_dir)
+            mu.create_sphere(dipole_loc, 0.15, layers_array, dipole_obj_name)
+            dipole_obj = bpy.data.objects[dipole_obj_name]
+            direction_obj = mu.cylinder_between(
+                dipole_loc, dipole_loc + dipole_dir, 0.1, layers_array, '{}_direction'.format(dipole_obj_name), color)
+            for obj in [dipole_obj, direction_obj]:
+                obj.select = True
+                obj.parent = parent_obj
             mu.create_and_set_material(dipole_obj)
-            global_dipole_ind += 1
+            mmvt.where_am_i.set_cursor_location(dipole_loc + dipole_dir)
     bpy.context.scene.layers[mmvt.MEG_LAYER] = True
 
 def delete_dipoles():
@@ -58,11 +66,14 @@ class DeleteDipoles(bpy.types.Operator):
 
 def draw(self, context):
     layout = self.layout
+
+    layout.prop(context.scene, 'dipoles_show_as_arrow', text="Plot as arrow")
     layout.prop(context.scene, 'dipoles_names', text="")
     layout.operator(DeleteDipoles.bl_idname, text="Delete Dipoles", icon='FORCE_HARMONIC')
 
 
 bpy.types.Scene.dipoles_names = bpy.props.EnumProperty(items=[], description="Dipoles names")
+bpy.types.Scene.dipoles_show_as_arrow = bpy.props.BoolProperty()
 
 
 def init(mmvt):
@@ -74,7 +85,7 @@ def init(mmvt):
     ScriptsPanel.dipoles_dict = dipoles_dict = mu.load(dipoles_fname)
     dipoles_items = sorted([(dipole_name, dipole_name, '', ind) for ind, dipole_name in enumerate(dipoles_dict.keys())])
     bpy.types.Scene.dipoles_names = bpy.props.EnumProperty(
-        items=dipoles_items, description="Dipoles names")
+        items=dipoles_items, description="Dipoles names", update=dipole_names_update)
     register()
 
 
@@ -94,3 +105,4 @@ def unregister():
 
 
 
+#
